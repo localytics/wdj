c6353ee6f4b99017c17aa5f4e59473733d7d9354
Brian Zeligson <bzeligson@localytics.com>
2016-01-31 02:14:35 -0500

    scaladoc

---
+++ index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"com.localytics" : [{"trait" : "com\/localytics\/HasWDJ.html", "name" : "com.localytics.HasWDJ"}, {"object" : "com\/localytics\/WDJ$.html", "name" : "com.localytics.WDJ"}], "scalaz.syntax" : [], "com.localytics.syntax" : [{"object" : "com\/localytics\/syntax\/wdj$.html", "name" : "com.localytics.syntax.wdj"}], "com" : [], "scalaz" : []};
--- project/plugins.sbt
+++
@@ -1,2 +0,0 @@
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
-addSbtPlugin("org.tpolecat" % "tut-plugin" % "0.4.0")
